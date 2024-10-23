5b779d3f1b2d1c99fbd495ee79061e548e8d1c78
bottolo <samuele.castiglia.98@gmail.com>
2024-10-23 10:53:59 +0200

    feat: Initial commit + added cursor and 3D libraries

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Cursor } from "./components/Cursor.tsx";
+
+function App() {
+	return (
+		<div className="w-screen h-screen bg-black cursor-none">
+			<Cursor />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Cursor.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+
+interface CursorProps {
+	defaultColor?: string;
+	size?: number;
+	thickness?: number;
+	lineThickness?: number;
+}
+export function Cursor({
+	defaultColor = "white",
+	size = 24,
+	thickness = 2,
+	lineThickness = 1,
+}: CursorProps) {
+	const [position, setPosition] = useState({ x: 0, y: 0 });
+	const [currentColor, setCurrentColor] = useState(defaultColor);
+
+	useEffect(() => {
+		const updatePosition = (e: { clientX: number; clientY: number }) => {
+			setPosition({ x: e.clientX, y: e.clientY });
+
+			const element = document.elementFromPoint(e.clientX, e.clientY);
+			if (element) {
+				const elementColor = element.getAttribute("data-cursor-color");
+				setCurrentColor(elementColor || defaultColor);
+			}
+		};
+
+		window.addEventListener("mousemove", updatePosition);
+
+		return () => {
+			window.removeEventListener("mousemove", updatePosition);
+		};
+	}, [defaultColor]);
+
+	return (
+		<div className="fixed inset-0 pointer-events-none z-50">
+			{/* Horizontal line */}
+			<div
+				className="absolute w-full"
+				style={{
+					top: `${position.y}px`,
+					height: `${lineThickness}px`,
+					backgroundColor: currentColor,
+				}}
+			/>
+
+			{/* Vertical line */}
+			<div
+				className="absolute h-full"
+				style={{
+					left: `${position.x}px`,
+					width: `${lineThickness}px`,
+					backgroundColor: currentColor,
+				}}
+			/>
+
+			{/* Hollow square cursor */}
+			<div
+				className="absolute"
+				style={{
+					left: `${position.x - size / 2 + 12.5}px`,
+					top: `${position.y - size / 2 + 12.5}px`,
+					width: `${size}px`,
+					height: `${size}px`,
+					border: `${thickness}px solid ${currentColor}`,
+					transform: "translate(-50%, -50%)",
+				}}
+			/>
+		</div>
+	);
+}
